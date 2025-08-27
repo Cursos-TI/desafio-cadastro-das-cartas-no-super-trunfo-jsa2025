be18557985ab8c1a750aa62cc5b9c485145a44c7
jsa2025 <wellington_pb@hotmail.com>
2025-08-27 19:07:14 +0000

    correnção do códio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
- 
+
 int main() {
     char Carta[20] = "A01";
     char Estado = 'A';
     char Cidade[20] = "Itaporanga";
-    int  População = 25000;
-    float Àrea  =  1521.11;
+    int Populacao = 25000; // Corrigido o uso de caracteres especiais no nome da variável
+    float Area = 1521.11;  // Corrigido o uso de caracteres especiais no nome da variável
     double PIB = 12345.67;
-    int pontoturistico = 3;
-    
+    int PontoTuristico = 3; // Ajustado para seguir o padrão de nomenclatura
+
     printf("Carta: %s\n", Carta);
     printf("Estado: %c\n", Estado);
     printf("Cidade: %s\n", Cidade);
-    printf("População: %d\n", População);
-    printf("Àrea: %.2f km²\n", Àrea);
-    printf("PIB: %.2f reais\n",PIB);
-    printf("ponto turistico: %d\n", pontoturistico);
-    
-    
- 
- 
+    printf("População: %d\n", Populacao);
+    printf("Área: %.2f km²\n", Area);
+    printf("PIB: %.2f reais\n", PIB);
+    printf("Pontos turísticos: %d\n", PontoTuristico);
+
     return 0;
-    
 }
